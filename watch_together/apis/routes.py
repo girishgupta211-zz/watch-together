faa746bcff3bdea38b0d2960fe9fea2f6a004bf5
Piyush Chourasiya <piyush.chourasiya@hotstar.com>
2019-04-16 14:25:37 +0530

    add create group api skeleton

---
+++ watch_together/apis/routes.py
@@ -0,0 +1,49 @@
+"""
+v1 version apis
+"""
+
+from flask import current_app, request
+from flask_restplus import Resource
+
+from watch_together.apis import api, ns
+from watch_together.utils.custom_exceptions import (
+    PayloadParseError, InvalidPayloadError, MissingKeysError)
+from watch_together.utils.payload_processing import (
+    parse_payload, check_required_keys
+)
+from watch_together.utils.response import response
+
+
+@ns.route('/create/group')
+class CreateGroup(Resource):
+    """
+    API for group creation
+    """
+    @api.doc(False)
+    def post(self):
+        """
+        Create Group
+        Method: POST
+        Sample payload:
+            {
+                "group_name": "",
+                "start_time": "",
+                "video_url": "",
+                "users_list": []
+            }
+        """
+        try:
+            # Parse Payload
+            payload = parse_payload(request)
+            # Check required params
+            check_required_keys(payload, [])
+            # create group here
+
+        except (InvalidPayloadError, PayloadParseError,
+                MissingKeysError) as err:
+            current_app.logger.error(
+                'POST: ml channel master data error: %s' % err.to_dict()
+            )
+            return response(error_dict=err.to_dict())
+
+        return response(data=payload)
